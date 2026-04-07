d4e093f9f7e38555eb04c4d20c3683f80fa919f3
jocarpe <114078928+chemacarceller@users.noreply.github.com>
2026-04-07 09:20:45 +0200

    Add files via upload

--- LevelManager.gd
+++ LevelManager.gd
@@ -1,21 +1,24 @@
 extends Node
 
-var actual_level: Node = null
+var actual_level : Node3D = null
 
 func load_new_level(scene_path: String):
+	
 	if (ResourceLoader.exists(scene_path)) :
+		
 		# The first time we change the level there is no reference to the current_scene
 		if actual_level == null : actual_level = get_tree().current_scene
+		
 		for child in actual_level.get_children():
 			# 'notification' sends the message ONLY to this specific node without going down to its own children.
-			MyLogger.info("Changing Level from " + str(actual_level) + ": Notification sent only to :" + child.name,'level_manager.gd', 11, true)
+			MyLogger.info("Changing Level from " + str(actual_level) + ": Notification sent only to :" + child.name,'level_manager.gd', 14, true)
 			child.notification(NOTIFICATION_WM_CLOSE_REQUEST)
 
 		# awaiting to the next frame so that the notifications sent are processed
 		await get_tree().process_frame
 
-		# queue_free the actual_level
-		actual_level.queue_free()
+		# free the actual_level
+		actual_level.free()
 
 		# From the event manager, all references to the previous level must be removed.
 		EventBus._reset()
@@ -28,6 +31,23 @@ func load_new_level(scene_path: String):
 		# Showing the hud messages
 		GameInstance._on_timer_timeout(false)
 
-		MyLogger.info("The actual level is : " + str(actual_level),'level_manager.gd',31)
+		MyLogger.info("The actual level is : " + str(actual_level),'level_manager.gd',34, true)
 	else :
-		MyLogger.error("The actual level : " + str(actual_level) + " cannot be loaded",'level_manager.gd',33)
+		MyLogger.error("The actual level : " + str(actual_level) + " cannot be loaded",'level_manager.gd',36, true)
+
+
+# How to handle a save quiting in the LevelManager
+func _notification(what) :
+	
+	if what == NOTIFICATION_WM_CLOSE_REQUEST :
+
+		MyLogger.info("Exiting LevelManager ...", 'LevelManager.gd', 40, true)
+		for child in actual_level.get_children():
+			# 'notification' sends the message ONLY to this specific node without going down to its own children.
+			MyLogger.info("Exiting LevelManager ... " + str(actual_level) + " : Notification sent only to :" + child.name,'level_manager.gd', 47, true)
+			child.notification(NOTIFICATION_WM_CLOSE_REQUEST)
+
+		# awaiting to the next frame so that the notifications sent are processed
+		await get_tree().process_frame
+
+		actual_level.queue_free()
